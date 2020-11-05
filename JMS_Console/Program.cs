7b8d13911b9eb93ac865eed9f6d897278ce89c17
S00177754 <S00177754@mail.itsligo.ie>
2020-11-05 18:33:25 +0000

    -Fixed Formatting of menu to be more compact and easier to read - Removed remenants of FirebasePostBuilder helper class

--- JMS_Console/Program.cs
+++ JMS_Console/Program.cs
@@ -51,30 +51,10 @@ namespace JMS_Console
         static public void PrintOptions()
         {
             Console.WriteLine("\nEnter a number from the list of options:");
-            Console.WriteLine("1. Get All Jobs");
-            Console.WriteLine("2. Get All Clients");
-            Console.WriteLine("3. Get All Equipment");
-            Console.WriteLine("4. Get All Employees");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("5. Get Job");
-            Console.WriteLine("6. Get Client");
-            Console.WriteLine("7. Get Equipment");
-            Console.WriteLine("8. Get Employee");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("9. Create Job");
-            Console.WriteLine("10. Create Client");
-            Console.WriteLine("11. Create Equipment");
-            Console.WriteLine("12. Create Employee");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("13. Update Job");
-            Console.WriteLine("14. Update Client");
-            Console.WriteLine("15. Update Equipment");
-            Console.WriteLine("16. Update Employee");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("17. Delete Job");
-            Console.WriteLine("18. Delete Client");
-            Console.WriteLine("19. Delete Equipment");
-            Console.WriteLine("20. Delete Employee");
+            Console.WriteLine("1. Get All Jobs\t\t5. Get Job\t\t9. Create Job\t\t13. Update Job\t\t17. Delete Job");
+            Console.WriteLine("2. Get All Clients\t6. Get Client\t\t10. Create Client\t14. Update Client\t18. Delete Client");
+            Console.WriteLine("3. Get All Equipment\t7. Get Equipment\t11. Create Equipment\t15. Update Equipment\t19. Delete Equipment");
+            Console.WriteLine("4. Get All Employees\t8. Get Employee\t\t12. Create Employee\t16. Update Employee\t20. Delete Employee");
             Console.WriteLine("-----------------------");
             Console.WriteLine("0. Exit");
             Console.Write("\nEnter number: ");
@@ -204,25 +184,33 @@ namespace JMS_Console
 
         public static void PrintJob(JobDTO dto)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"ID:{dto.ID} - Name:{dto.Name} - Client ID:{dto.ClientID}");
             Console.WriteLine("Equipment List:");
             dto.EquipmentList.ForEach(e => Console.WriteLine(e));
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine();
         }
 
         public static void PrintClient(ClientDTO dto)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"ID:{dto.ID} - Company Name:{dto.CompanyName} - Address:{dto.Address}");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void PrintEmployee(EmployeeDTO dto)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"ID:{dto.ID} - First Name:{dto.FirstName} - Last Name:{dto.LastName} - Role: {(EmployeeRole)dto.Role}");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         public static void PrintEquipment(EquipmentDTO dto)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"ID:{dto.ID} - Name:{dto.Name} - Model:{dto.Model} - Manufacturer:{dto.Manufacturer}");
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
         #endregion
@@ -296,11 +284,11 @@ namespace JMS_Console
             Console.Write("Please enter name of Job to add:");
             job.Name = Console.ReadLine();
 
-            Console.WriteLine("Please enter ID of client:");
+            Console.Write("Please enter ID of client:");
             job.ClientID = Console.ReadLine();
             job.ID = Guid.NewGuid().ToString();
 
-            Console.WriteLine("Please enter ID's of equipment for job seperated by commas:");
+            Console.Write("Please enter ID's of equipment for job seperated by commas:");
             string idList = Console.ReadLine();
 
             job.EquipmentList.AddRange(idList.Split(','));
@@ -314,7 +302,7 @@ namespace JMS_Console
             ClientDTO client = new ClientDTO();
             client.CompanyName = Console.ReadLine();
 
-            Console.WriteLine("Please enter address of client:");
+            Console.Write("Please enter address of client:");
             client.Address = Console.ReadLine();
             client.ID = Guid.NewGuid().ToString();
 
@@ -327,10 +315,10 @@ namespace JMS_Console
             EquipmentDTO equipment = new EquipmentDTO();
             equipment.Name = Console.ReadLine();
 
-            Console.WriteLine("Please enter manufacturer of equipment:");
+            Console.Write("Please enter manufacturer of equipment:");
             equipment.Manufacturer = Console.ReadLine();
 
-            Console.WriteLine("Please enter model of equipment:");
+            Console.Write("Please enter model of equipment:");
             equipment.Model = Console.ReadLine();
 
             equipment.ID = Guid.NewGuid().ToString();
@@ -344,7 +332,7 @@ namespace JMS_Console
             Console.Write("Please enter first name of Employee to add:");
             employee.FirstName = Console.ReadLine();
 
-            Console.WriteLine("Please enter last name of employee:");
+            Console.Write("Please enter last name of employee:");
             employee.LastName = Console.ReadLine();
 
             employee.Role = 0;
@@ -356,7 +344,7 @@ namespace JMS_Console
 
         static void MenuOptions_UpdateJob()
         {
-            Console.WriteLine("Please enter the id of the object you would like to update:");
+            Console.Write("Please enter the id of the object you would like to update:");
             JobDTO dto = JMS_Commands.GetJob(Console.ReadLine());
             Console.WriteLine("Job to update:");
             PrintJob(dto);
@@ -364,14 +352,14 @@ namespace JMS_Console
             Console.Write("Please enter updated name of Job to add:");
             dto.Name = Console.ReadLine();
 
-            Console.WriteLine("Please enter updated ID of client:");
+            Console.Write("Please enter updated ID of client:");
             dto.ClientID = Console.ReadLine();
             dto.ID = Guid.NewGuid().ToString();
 
             string eqList = "";
             dto.EquipmentList.ForEach(e => eqList = string.Concat(eqList, e, ","));
             Console.WriteLine(string.Concat("\n","Current IDs:",eqList));
-            Console.WriteLine("Please enter ID's of equipment for job seperated by commas:");
+            Console.Write("Please enter ID's of equipment for job seperated by commas:");
             string idList = Console.ReadLine();
             dto.EquipmentList.AddRange(idList.Split(','));
 
@@ -380,15 +368,15 @@ namespace JMS_Console
 
         static void MenuOptions_UpdateClient()
         {
-            Console.WriteLine("Please enter the id of the object you would like to update:");
+            Console.Write("Please enter the id of the object you would like to update:");
             ClientDTO client = JMS_Commands.GetClient(Console.ReadLine());
-            Console.WriteLine("Client to update:");
+            Console.Write("Client to update:");
             PrintClient(client);
 
             Console.Write("Please enter updated name of Client to add:");
             client.CompanyName = Console.ReadLine();
 
-            Console.WriteLine("Please enter updated address of client:");
+            Console.Write("Please enter updated address of client:");
             client.Address = Console.ReadLine();
 
             JMS_Commands.UpdateClient(client);
@@ -396,18 +384,18 @@ namespace JMS_Console
 
         static void MenuOptions_UpdateEquipment()
         {
-            Console.WriteLine("Please enter the id of the equipment item you would like to update:");
+            Console.Write("Please enter the id of the equipment item you would like to update:");
             EquipmentDTO equipment = JMS_Commands.GetEquipment(Console.ReadLine());
-            Console.WriteLine("Equipment to update:");
+            Console.Write("Equipment to update:");
             PrintEquipment(equipment);
 
             Console.Write("Please enter updated name of Equipment to add:");
             equipment.Name = Console.ReadLine();
 
-            Console.WriteLine("Please enter updated manufacturer of equipment:");
+            Console.Write("Please enter updated manufacturer of equipment:");
             equipment.Manufacturer = Console.ReadLine();
 
-            Console.WriteLine("Please enter updated model of equipment:");
+            Console.Write("Please enter updated model of equipment:");
             equipment.Model = Console.ReadLine();
 
             JMS_Commands.UpdateEquipment(equipment);
@@ -415,15 +403,15 @@ namespace JMS_Console
 
         static void MenuOptions_UpdateEmployee()
         {
-            Console.WriteLine("Please enter the id of the equipment item you would like to update:");
+            Console.Write("Please enter the id of the equipment item you would like to update:");
             EmployeeDTO employee = JMS_Commands.GetEmployee(Console.ReadLine());
-            Console.WriteLine("Employee to update:");
+            Console.Write("Employee to update:");
             PrintEmployee(employee);
 
             Console.Write("Please enter updated first name of Employee to add:");
             employee.FirstName = Console.ReadLine();
 
-            Console.WriteLine("Please enter updated last name of employee:");
+            Console.Write("Please enter updated last name of employee:");
             employee.LastName = Console.ReadLine();
 
             employee.Role = 0;
